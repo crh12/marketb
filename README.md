9503911efd2da20d43420cc7923d782c03c4211e
crh12 <scrin012@gmail.com>
2025-03-12 15:08:06 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# marketb
+## 폰트
+
+### 메인 제목 32
+font-size: 2rem; font-weight: 500; letter-spacing: -0.02em;
+### 내용 제목 24
+font-size: 1.50rem; font-weight: 500; letter-spacing: -0.02em;
+### 메인 중제목 20
+font-size: 1.25rem; font-weight: 500;
+### 내용 18 강조
+font-size: 1.13rem; font-weight: 600; letter-spacing: -0.02em;
+### 내용 18
+font-size: 1.13rem; letter-spacing: -0.02em;
+### 메뉴 17 강조
+font-size: 1.06rem; font-weight: 600; letter-spacing: -0.02em;
+### 메뉴 17
+font-size: 1.06rem; letter-spacing: -0.02em;
+### 헤더 15
+font-size: 0.94rem; font-weight: 500; letter-spacing: -0.02em;
+### 내용 15 강조
+font-size: 0.94rem; font-weight: 600; letter-spacing: -0.02em;
+### 내용 15 
+font-size: 0.94rem; letter-spacing: -0.02em;
+### 리뷰 내용 14 140%
+font-size: 0.88rem; letter-spacing: -0.02em; line-height: 1.4;
+### 보조 12 강조
+font-size: 0.75rem; font-weight: 500; letter-spacing: -0.02em;
+### 보조 12
+font-size: 0.75rem; letter-spacing: -0.02em;
+### 보조 9 강조
+font-size: 0.56rem; font-weight: 600; letter-spacing: -0.02em;
---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+const mainSlideCurrent = document.querySelector('.main_bnr .swiper_current');
+const mainSlideTotal = document.querySelector('.main_bnr .swiper_total');
+const rank100 = document.querySelectorAll('.rank100 a');
+const rank100Img = document.querySelectorAll('.rank100 a .img_wrap');
+const mdCate = document.querySelectorAll('.md_ctnr .category a');
+const roomWrapper = document.querySelector('.room_swiper .swiper-wrapper');
+const roomSlides = document.querySelectorAll('.room_swiper .swiper-wrapper .swiper-slide');
+
+let currentIndex = 0;
+
+
+function activeReset(name){
+    for(let i of name) i.classList.remove('active');
+};
+
+
+mainSlideTotal.textContent = `0${document.querySelectorAll('.main_bnr .swiper-slide').length}`;
+
+const mainSwiper = new Swiper('.main_swiper',{
+    autoplay:{delay:2500,},
+    loop:true,
+    spaceBetween:50,
+    slidesPerView: 1.1,
+    centeredSlides:true,
+    navigation:{
+        nextEl:'.main_bnr .next',
+        prevEl:'.main_bnr .prev',
+    },
+    on:{
+        slideChange:function(){
+            mainSlideCurrent.textContent = `0${this.realIndex+1}`;
+        }
+    },
+    scrollbar: {
+        el: '.main_bnr .swiper-scrollbar',
+    },
+});
+
+const rankTimer = setInterval(()=>{
+    if(currentIndex == rank100.length - 1){ /* index 초기화 */
+        setTimeout(()=>{
+            currentIndex = 0;
+            activeReset(rank100);
+            rank100[0].classList.add('active')
+        }, 0)
+    } else {
+        activeReset(rank100);
+        currentIndex++;
+    };
+    rank100[currentIndex].classList.add('active');
+}, 3000);
+
+for(let i of mdCate){
+    i.addEventListener('click',(e)=>{
+        e.preventDefault();
+        activeReset(mdCate);
+        i.classList.add('active')
+    });
+};
+
+for (let i of roomSlides){
+    const clone = i.cloneNode(true);
+    roomWrapper.appendChild(clone);
+};
+
+const roomSwiper = new Swiper('.room_swiper',{
+    autoplay:{delay:2500,},
+    loop:true,
+    centeredSlides:true,
+    slidesPerView: 1.01,
+    spaceBetween:30,
+    navigation:{
+        nextEl:'.room_swiper .swiper-button-next',
+        prevEl:'.room_swiper .swiper-button-prev',
+    },
+});
