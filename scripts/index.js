224ce3a4bda3c5fe467c1407053d0ba7bbbd4cb4
crh12 <scrin012@gmail.com>
2025-03-26 19:28:35 +0900

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-const mainSlideCurrent = document.querySelector('.main_bnr .swiper_current');
-const mainSlideTotal = document.querySelector('.main_bnr .swiper_total');
+const mainSlideCurrent = document.querySelector('#main_bnr .swiper_current');
+const mainSlideTotal = document.querySelector('#main_bnr .swiper_total');
 const rank100 = document.querySelectorAll('.rank100 a');
 const rank100Img = document.querySelectorAll('.rank100 a .img_wrap');
-const mdCate = document.querySelectorAll('.md_ctnr .category a');
+const mdCate = document.querySelectorAll('#md_ctnr .category a');
 const roomWrapper = document.querySelector('.room_swiper .swiper-wrapper');
 const roomSlides = document.querySelectorAll('.room_swiper .swiper-wrapper .swiper-slide');
 
@@ -14,7 +14,7 @@ function activeReset(name){
 };
 
 
-mainSlideTotal.textContent = `0${document.querySelectorAll('.main_bnr .swiper-slide').length}`;
+mainSlideTotal.textContent = `0${document.querySelectorAll('#main_bnr .swiper-slide').length}`;
 
 const mainSwiper = new Swiper('.main_swiper',{
     autoplay:{delay:2500,},
@@ -23,8 +23,8 @@ const mainSwiper = new Swiper('.main_swiper',{
     slidesPerView: 1.1,
     centeredSlides:true,
     navigation:{
-        nextEl:'.main_bnr .next',
-        prevEl:'.main_bnr .prev',
+        nextEl:'#main_bnr .next',
+        prevEl:'#main_bnr .prev',
     },
     on:{
         slideChange:function(){
@@ -32,7 +32,7 @@ const mainSwiper = new Swiper('.main_swiper',{
         }
     },
     scrollbar: {
-        el: '.main_bnr .swiper-scrollbar',
+        el: '#main_bnr .swiper-scrollbar',
     },
 });
 
@@ -64,7 +64,7 @@ for (let i of roomSlides){
 };
 
 const roomSwiper = new Swiper('.room_swiper',{
-    autoplay:{delay:2500,},
+    autoplay:{delay:5000,},
     loop:true,
     centeredSlides:true,
     slidesPerView: 1.01,
